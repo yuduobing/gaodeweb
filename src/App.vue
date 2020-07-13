48281c352aa51edca03f97dc26729853f7fecaef
余多彬 <1510557673@qq.com>
2020-07-13 09:37:40 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+	{{numAnim}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      numAnim:'高得软件'
+    }
+  }
+}
+</script>
